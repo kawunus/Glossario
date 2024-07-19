8795d2c30a9cbb8328637cda52d5c59a765115bb
arbuz <73398457+Markovtsev1@users.noreply.github.com>
2024-07-19 17:01:39 +0300

    Создана разметка activity_register.xml

--- app/src/main/java/com/kawunus/glossario/RegisterActivity.kt
+++ app/src/main/java/com/kawunus/glossario/RegisterActivity.kt
@@ -6,9 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
