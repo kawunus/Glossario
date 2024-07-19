f0f4fe03e7b823db6f97de9eb941ded2f99f8008
arbuz <73398457+Markovtsev1@users.noreply.github.com>
2024-07-19 21:00:01 +0300

    добавлен класс AccountHelper, в нем добавлена функция для регистрации по email

--- app/src/main/java/AccountHelper/AccountHelper.kt
+++ app/src/main/java/AccountHelper/AccountHelper.kt
@@ -1,6 +1,34 @@
 package AccountHelper
 
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.kawunus.glossario.R
 import com.kawunus.glossario.RegisterActivity
 
-class AccountHelper() {
+class AccountHelper(private val act: RegisterActivity) {
+    private lateinit var signInClient: GoogleSignInClient
+
+    fun signUpWithEmail(
+        email: String,
+        password: String,
+        birthdayDate: String
+    ) {
+        if (email.isNotEmpty() && password.isNotEmpty() && birthdayDate.isNotEmpty()) {
+            act.mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+
+                    } else {
+                        Toast.makeText(
+                            act,
+                            act.getString(R.string.sign_up_error_firebase),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        } else {
+            Toast.makeText(act, act.getString(R.string.sign_up_error_empty), Toast.LENGTH_LONG)
+                .show()
+        }
+    }
 }
--- app/src/main/java/com/kawunus/glossario/RegisterActivity.kt
+++ app/src/main/java/com/kawunus/glossario/RegisterActivity.kt
@@ -1,12 +1,16 @@
 package com.kawunus.glossario
 
+import AccountHelper.AccountHelper
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.kawunus.glossario.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    val mAuth = FirebaseAuth.getInstance()
+    private val helper = AccountHelper(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         init()
@@ -16,12 +20,17 @@ class RegisterActivity : AppCompatActivity() {
             finish()
         }
 
-        if (intent.getStringExtra("TYPE") == "login")
-        {
-          updateUi()
-        }
-        else {
+        if (intent.getStringExtra("TYPE") == "login") {
+            updateUi()
+        } else {
 
+            binding.signUpButton.setOnClickListener {
+                helper.signUpWithEmail(
+                    binding.emailEditText.text.toString(),
+                    binding.passwordEditText.text.toString(),
+                    binding.birthdayEditText.text.toString()
+                )
+            }
         }
     }
 
@@ -29,8 +38,7 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
     }
 
-    private fun updateUi() = with(binding)
-    {
+    private fun updateUi() = with(binding) {
         titleTextView.setText(R.string.sign_in_with_email)
         birthdayEditText.visibility = View.GONE
         birthdayTextView.visibility = View.GONE
