96c87702c6d22c4d6db7c3e65b6dd0b1f4da59f3
arbuz <73398457+Markovtsev1@users.noreply.github.com>
2024-07-21 14:19:58 +0300

    В MainActivity добавлен bottomNavigationBar

--- app/src/main/java/com/kawunus/glossario/MainActivity.kt
+++ app/src/main/java/com/kawunus/glossario/MainActivity.kt
@@ -4,24 +4,47 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.kawunus.glossario.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var prefs: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
-        init()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        init()
+        setContentView(binding.root)
+        replaceFragment(Home())
         if (prefs.getString("status", "noreg") == "noreg") {
             val intent = Intent(this@MainActivity, StartActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.home -> replaceFragment(Home())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {
+
+                }
+
+            }
+            true
         }
     }
 
     private fun init() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         prefs = getSharedPreferences("profile", MODE_PRIVATE)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
