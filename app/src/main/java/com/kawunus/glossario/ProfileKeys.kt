32e3d336148a58ad2c2cc6c5968f52f76c9c0513
arbuz <73398457+Markovtsev1@users.noreply.github.com>
2024-07-22 22:42:05 +0300

    Вынес из PrefKeys ProfileKeys

---
+++ app/src/main/java/com/kawunus/glossario/ProfileKeys.kt
@@ -0,0 +1,14 @@
+package com.kawunus.glossario
+
+
+object ProfileKeys {
+    const val USER_EMAIL = "email"
+    const val USER_NICKNAME = "nickname"
+    const val USER_STATUS = "status"
+    const val USER_IMAGE = "profileImage"
+
+    object UserStatus {
+        const val REGISTER = "reg"
+        const val NOT_REGISTER = "noreg"
+    }
+}
