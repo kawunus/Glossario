fd04aaac7d6077c0720adcb26588ce2705714a47
arbuz <73398457+Markovtsev1@users.noreply.github.com>
2024-07-23 16:15:34 +0300

    добавил работоспособность кнопкам в SettingsActivity

--- app/src/main/java/com/kawunus/glossario/SettingsActivity.kt
+++ app/src/main/java/com/kawunus/glossario/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.kawunus.glossario
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kawunus.glossario.databinding.ActivitySettingsBinding
@@ -14,6 +16,36 @@ class SettingsActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             finish()
         }
+
+        binding.shareView.setOnClickListener{
+            binding.shareView.setOnClickListener {
+                val intent = Intent(Intent.ACTION_SEND)
+                val message = getString(R.string.settings_share_message)
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/plain"
+                val chooser = Intent.createChooser(intent, getString(R.string.settings_share_title))
+                startActivity(chooser)
+            }
+        }
+        binding.supportView.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.dev_email)))
+            startActivity(intent)
+        }
+        binding.telegramView.setOnClickListener{
+            val url = Uri.parse(getString(R.string.settings_chanel))
+            val intent = Intent(Intent.ACTION_VIEW, url)
+
+            startActivity(intent)
+        }
+        binding.gitView.setOnClickListener{
+            val url = Uri.parse(getString(R.string.settings_git))
+            val intent = Intent(Intent.ACTION_VIEW, url)
+
+            startActivity(intent)
+        }
+
     }
 
     fun init() {
